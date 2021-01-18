b574d1494cdc177dffe39f807d5457c3e32c8594
Daniel B <danielbuompane@gmail.com>
2021-01-18 16:31:17 -0500

    extract handleReset function out

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -12,7 +12,7 @@ import {
 
 function PokemonInfo({pokemonName}) {
   const [{status, pokemon, error}, setState] = React.useState({
-    status: 'idle',
+    status: pokemonName ? 'pending' : 'idle',
     pokemon: null,
     error: null,
   })
@@ -61,15 +61,16 @@ function App() {
     setPokemonName(newPokemonName)
   }
 
+  const handleReset = () => {
+    setPokemonName('')
+  }
+
   return (
     <div className="pokemon-info-app">
       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
       <hr />
       <div className="pokemon-info">
-        <ErrorBoundary
-          FallbackComponent={ErrorFallback}
-          onReset={() => setPokemonName('')}
-        >
+        <ErrorBoundary FallbackComponent={ErrorFallback} onReset={handleReset}>
           <PokemonInfo pokemonName={pokemonName} />
         </ErrorBoundary>
       </div>
